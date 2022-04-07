634800d386079b3c6dab9c1e04dd3a69498699f2
PabloHassan <95355922+Pablohassan@users.noreply.github.com>
2022-04-07 17:41:47 +0200

    notre main page (#2)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import ButtonPlay from "@components/ButtonPlay";
 
 export default function Home() {
-  return <ButtonPlay />;
+  return (
+    <>
+      <header className="App-header"> Un Header</header>
+      <body id="ombreH1">
+        Notre Body ici
+        <h1>Countrizz</h1>
+        <div>
+          <ButtonPlay />
+        </div>
+      </body>
+      <footer>Un footer</footer>
+    </>
+  );
 }

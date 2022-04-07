ae9532930e23fcb3d36e6a20bcb76d220d39802f
MarcHazime <89213301+MarcHazime@users.noreply.github.com>
2022-04-07 16:03:24 +0200

    Creation boutton play (#1)
    Add play button

---
+++ frontend/src/components/ButtonPlay.jsx
@@ -0,0 +1,14 @@
+import "@components/ButtonPlay.css";
+
+export default function ButtonPlay() {
+  return (
+    <div>
+      <a
+        className="playGame"
+        href="https://www.youtube.com/watch?v=XasQLCjRrDE"
+      >
+        Play
+      </a>
+    </div>
+  );
+}
